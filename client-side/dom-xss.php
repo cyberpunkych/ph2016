70b0ef52f8295229903408a5d5f8d759b461ba34
cyberpunkych <cyber-punk@xakep.ru>
2016-05-18 15:32:02 +0300

    new tasks
    Added Client-Side, SSRF and LFI

---
+++ client-side/dom-xss.php
@@ -0,0 +1,4 @@
+<script>
+var user = eval(location.hash.slice(1));
+</script>
+<button onclick="alert(user)">click me!</button>
---
+++ ssrf/index.php
@@ -0,0 +1,19 @@
+<form type="GET" action="#">
+<input name="url" type="text">
+<input type="submit">
+</form>
+<?php
+
+$image = $_GET['url'];
+$ch = curl_init();
+$optArray = array(
+    CURLOPT_URL => $image,
+    CURLOPT_RETURNTRANSFER => true
+);
+curl_setopt_array($ch, $optArray);
+$imageData = base64_encode(curl_exec($ch));
+curl_close($ch);
+$src = 'data:;base64,'.$imageData;
+echo '<img src="',$src,'">';
+
+?>
